d854f768bf578069bb80b171d8bb380d6bf63ef0
Radhakinjangi <kinjangiradha518@gmail.com>
2023-02-09 21:58:20 +0530

    5

--- SingleInheritance.java
+++ SingleInheritance.java
@@ -9,9 +9,28 @@ class Fox extends Animal{
 class BabyDog extends Fox{
     void weep(){System.out.println("weeping...");}
 }
+class Cat extends Animal{
+    void meow(){System.out.println("meowing...");}
+}
+///multiple inheritance:
+//class A{
+//    void msg(){System.out.println("Hello");}
+//}
+//class B{
+//    void msg(){System.out.println("Welcome");}
+//}
+//class C extends A,B{//suppose if it were
+//
+//    public static void main(String args[]){
+//        C obj=new C();
+//        obj.msg();//Now which msg() method would be invoked?
+//    }
+//}
 public class SingleInheritance {
     public static void main(String[] args) {
         BabyDog v = new BabyDog();
+        Cat c=new Cat();
+        c.meow();
         v.weep();
         v.bark();
         v.eat();
