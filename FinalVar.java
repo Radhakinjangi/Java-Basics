b40381d2ef81654ea3f105334097929bec67530e
Radhakinjangi <kinjangiradha518@gmail.com>
2023-02-09 18:58:43 +0530

    4-2

---
+++ FinalVar.java
@@ -0,0 +1,10 @@
+public class FinalVar {
+    public static void main(String args[])
+    {
+        final int i;
+        i = 20;
+        static final double PI = 3.141592653589793;
+        System.out.println(PI);
+        System.out.println(i);
+    }
+}
